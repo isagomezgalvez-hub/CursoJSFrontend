b4e0f21742bdf85d3fe89a8eb685b7c28fc893ef
Isa Gómez <hello@isagomez.com>
2021-02-11 22:00:44 +0100

    rendereizamos los tweets desde el controlador

---
+++ js/controllers/PostListController.js
@@ -0,0 +1,25 @@
+import BaseController from './BaseController.js';
+import dataService from '../services/DataService.js';
+import { tweetView } from '../views.js';
+
+export default class PostsListController extends BaseController {
+
+	render(tweets) {
+		for (const tweet of tweets) {
+			const article = document.createElement('article');
+			article.innerHTML = tweetView(tweet);
+			this.element.appendChild(article);
+		}
+	}
+
+	async loadPosts() {
+		try {
+			const tweets = await dataService.getTweets();
+			this.render(tweets);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+}
+
--- js/index.js
+++ js/index.js
@@ -1,35 +1,12 @@
-import { tweetView } from "./views.js";
-import dataService from "./services/DataService.js";
+import PostsListController from './controllers/PostListController.js';
 
 window.addEventListener("DOMContentLoaded", async (event) => {
 	const loader = document.querySelector(".lds-ring");
 	loader.classList.add("hidden");
 
-	const cargarTweets = (tweets) => {
-		console.log('BIEN! VOY A PINTAR LOS TWEETS');
-		const list = document.querySelector(".posts-list");
-		for (const tweet of tweets) {
-			const tweetElement = document.createElement("article");
-			const tweetHTML = tweetView(tweet);
-			tweetElement.innerHTML = tweetHTML;
-			list.appendChild(tweetElement);
-		}
-	};
-
-	const avisarDelError = (error) => {
-		console.error('NO SE HAN PODIDO CARGAR LOS TWEETS', error);
-	};
-	try {
-		const tweets = await dataService.getTweets()
-		cargarTweets(tweets);
-	} catch (error) {
-		avisarDelError(error)
-	}
-
-
-
-
-
-
+	const element = document.querySelector('.posts-list');
+	const controller = new PostsListController(element);
+	controller.loadPosts();
 
 });
+
