714f5d191f41692f54d4eab9fd34d9d45500fa2f
Isa Gómez <hello@isagomez.com>
2021-02-12 00:21:53 +0100

    Cerramos el mensaje de error haciendo click en el botón de cerrar

--- js/controllers/ErrorController.js
+++ js/controllers/ErrorController.js
@@ -6,7 +6,14 @@ export default class ErrorController extends BaseController {
 
 	showError(errorMessage) {
 		this.element.innerHTML = errorView(errorMessage);
-		this.element.classList.remove('hidden')
+		this.element.classList.remove('hidden');
+		this.element.addEventListener('click', (event) => {
+			console.log('CLIk para cerrar', event)
+			if (event.target === this.element || event.target.classList.contains('delete')) {
+				this.element.classList.add('hidden');
+			}
+
+		})
 	}
 
 	hideError() {
